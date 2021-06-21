478a9aedf8e73e142c212747f43a3973c04ed8d9
Axana <raiser.axana@gmail.com>
2021-06-21 18:21:27 +0200

    Added Python/SQL lab + restructured repo

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ## Contents:
 
+### Week 1-2
 1. Random fact about me
 2. My holiday preferences
 3. String operations lab
 4. Error handling lab
 5. Pandas labs (1-5)
+
+### Week 3
 6. SQL labs (1-5)
+
+### Week 4
+7. Python/SQL lab
--- Week_3/SQL_Lab.sql
+++ Week_3/SQL_Lab.sql
@@ -497,3 +497,196 @@ JOIN rental r
 USING(inventory_id)
 GROUP BY f.title
 ORDER BY 2 DESC;
+
+#############################################
+####### SQL Lab 5 (Friday) - Optional #######
+#############################################
+
+# 1. How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT 
+	f.title,
+    COUNT(*) AS count
+FROM film f
+JOIN inventory i
+USING(film_id)
+WHERE f.title = 'Hunchback Impossible';
+
+SELECT 
+    COUNT(*) as count
+FROM inventory
+WHERE film_id =
+(
+SELECT film_id
+FROM film
+WHERE title = 'Hunchback Impossible'
+);
+
+# 2. List all films whose length is longer than the average of all the films.
+SELECT *
+FROM film 
+WHERE length >
+(
+SELECT AVG(length)
+FROM film
+)
+ORDER BY length;
+
+# 3. Use subqueries to display all actors who appear in the film Alone Trip.
+SELECT 
+	actor_id,
+	first_name,
+    last_name
+FROM actor 
+WHERE actor_id IN
+(
+SELECT actor_id
+FROM film_actor
+WHERE film_id IN
+(
+SELECT film_id
+FROM film 
+WHERE title = 'Alone Trip'
+)
+);
+
+# 4. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+# Identify all movies categorized as family films.
+SELECT 
+	f.title
+FROM film f
+JOIN film_category fc
+USING(film_id)
+JOIN category c
+USING(category_id)
+WHERE c.name = 'family'
+ORDER BY 1;
+
+SELECT title
+FROM film
+WHERE film_id IN
+(
+SELECT film_id
+FROM film_category
+WHERE category_id IN
+(
+SELECT category_id
+FROM category
+WHERE name = 'family'
+)
+);
+
+# 5. Get name and email from customers from Canada using subqueries. Do the same with joins. 
+# Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, 
+# that will help you get the relevant information.
+
+SELECT 
+	first_name,
+    last_name,
+    email
+FROM customer 
+WHERE address_id IN
+(
+SELECT address_id
+FROM address 
+WHERE city_id IN
+(
+SELECT city_id
+FROM city
+WHERE country_id IN
+(
+SELECT country_id
+FROM country 
+WHERE country = 'Canada'
+)
+)
+);
+
+SELECT 
+	cu.first_name,
+    cu.last_name,
+    cu.email,
+	co.country
+FROM customer cu
+JOIN address a
+USING(address_id)
+JOIN city ci
+USING(city_id)
+JOIN country co
+USING(country_id)
+WHERE co.country = 'Canada';
+
+# 6. Which are films starred by the most prolific actor? 
+# Most prolific actor is defined as the actor that has acted in the most number of films. 
+# First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
+
+SELECT 
+	CONCAT(first_name, ' ', last_name) AS actor_name,
+    title,
+    release_year
+FROM actor
+JOIN film_actor
+USING(actor_id)
+JOIN film 
+USING(film_id)
+WHERE actor_id =
+(
+SELECT
+	actor_id
+FROM actor
+JOIN film_actor
+USING(actor_id)
+JOIN film
+USING (film_id)
+GROUP BY 1
+ORDER BY COUNT(film_id) DESC
+LIMIT 1
+);
+
+# 7. Films rented by most profitable customer. 
+# You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+SELECT 
+	film_id,
+    title,
+    amount
+FROM film
+JOIN inventory
+USING(film_id)
+JOIN rental
+USING(inventory_id)
+JOIN payment 
+USING(rental_id)
+WHERE rental.customer_id = 
+(
+SELECT 
+	customer_id
+FROM customer
+JOIN payment
+USING(customer_id)
+GROUP BY 1
+ORDER BY SUM(amount) DESC
+LIMIT 1
+)
+ORDER BY rental_date DESC;
+
+# 8. Customers who spent more than the average payments.
+SELECT 
+	customer_id,
+    SUM(amount) AS payment
+FROM customer
+JOIN payment 
+USING(customer_id)
+GROUP BY customer_id
+HAVING SUM(amount) >
+(
+SELECT 
+AVG(total_payment)
+FROM 
+(
+SELECT 
+	customer_id,
+	SUM(amount) AS total_payment
+FROM payment
+GROUP BY customer_id
+) t1
+)
+ORDER BY payment DESC;
